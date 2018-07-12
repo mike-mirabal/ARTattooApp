229659eac8aa1a0f0bfbc2e1bcadcc7bf95735d4
c0ncrete <3616624+c0ncrete@users.noreply.github.com>
2018-07-12 16:28:36 -0500

    Update SwitchCamera.cs
    I don't think the clip you showed me has anything to do with this script as the functionality seems quite simple. I did, however, weed out some unnecessary duplication. I'm guessing the issue has something to do with Vuforia. I'll poke around on Google and see what I can come up with.

--- Assets/DataFiles/Scripts/SwitchCamera.cs
+++ Assets/DataFiles/Scripts/SwitchCamera.cs
@@ -1,36 +1,33 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
 public class SwitchCamera : MonoBehaviour {
 
-    private bool on = true;
+    // default to front camera
+    private bool on = false;
 
-    public void Switch()
-    {
-        on = !on;
+    public void Switch() {
+
+        // moved outside of conditional to avoid duplication
+        CameraDevice.Instance.Stop();
+        CameraDevice.Instance.Deinit();
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+
+        // consider storing camera direction in a variable to move Init outside conditional as well
         if (on)
         {
-            CameraDevice.Instance.Stop();
-            CameraDevice.Instance.Deinit();
-            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
-
             CameraDevice.Instance.Init(CameraDevice.CameraDirection.CAMERA_BACK);
-            CameraDevice.Instance.Start();
-            TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
-
         }
-        else if (!on)
+        else
         {
-            CameraDevice.Instance.Stop();
-            CameraDevice.Instance.Deinit();
-
-            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
             CameraDevice.Instance.Init(CameraDevice.CameraDirection.CAMERA_FRONT);
-
-            CameraDevice.Instance.Start();
-            TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
         }
+
+        // moved outside of conditional to avoid duplication
+        CameraDevice.Instance.Start();
+        TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
     }
+
 }
