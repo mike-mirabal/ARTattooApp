6d8beae20638986552c8d0dd1a2e54c36b9ea41f
mike-mirabal <mikemirabal@hotmail.com>
2018-07-12 15:01:24 -0500

    Initial Commit

---
+++ Assets/DataFiles/Scripts/ChangeSceneMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneMenu : MonoBehaviour {
+
+
+    public void Backtomenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Startgame()
+    {
+        SceneManager.LoadScene("ARTattooNew");
+    }
+
+    public void Exitgame()
+    {
+        Application.Quit();
+        Debug.Log("Exit button pressed.");
+    }
+}
---
+++ Assets/DataFiles/Scripts/SwitchCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SwitchCamera : MonoBehaviour {
+
+    private bool on = true;
+
+    public void Switch()
+    {
+        on = !on;
+        if (on)
+        {
+            CameraDevice.Instance.Stop();
+            CameraDevice.Instance.Deinit();
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+
+            CameraDevice.Instance.Init(CameraDevice.CameraDirection.CAMERA_BACK);
+            CameraDevice.Instance.Start();
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
+
+        }
+        else if (!on)
+        {
+            CameraDevice.Instance.Stop();
+            CameraDevice.Instance.Deinit();
+
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Stop();
+            CameraDevice.Instance.Init(CameraDevice.CameraDirection.CAMERA_FRONT);
+
+            CameraDevice.Instance.Start();
+            TrackerManager.Instance.GetTracker<ObjectTracker>().Start();
+        }
+    }
+}
